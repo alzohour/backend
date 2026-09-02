b6909fac80ad1ae43f903824c572c97dddcd56ea
Ahmed Elgohary <ahmedelgoharyy7@gmail.com>
2026-09-02 17:55:15 +0300

    feat: sync static wwwroot with interactive leaflet map

---
+++ Backend/wwwroot/_next/static/chunks/31g-t82sdml_c.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,89144,s=>{s.v(t=>Promise.all(["static/chunks/0n8kzvw2z_6as.css","static/chunks/0tm0842ac_lh1.js"].map(t=>s.l(t))).then(()=>t(53345)))}]);
